b3e65cdac28b6a36ab0dd2777db303fddc816b5c
ceavinrufus <18221162@std.stei.itb.ac.id>
2025-05-06 20:04:46 +0700

    refactor: use database url instead

--- src/database/typeorm-config.service.ts
+++ src/database/typeorm-config.service.ts
@@ -10,12 +10,13 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
+      url: this.configService.get('database.url', { infer: true }),
       type: this.configService.get('database.type', { infer: true }),
-      host: this.configService.get('database.host', { infer: true }),
-      port: this.configService.get('database.port', { infer: true }),
-      username: this.configService.get('database.username', { infer: true }),
-      password: this.configService.get('database.password', { infer: true }),
-      database: this.configService.get('database.name', { infer: true }),
+      // host: this.configService.get('database.host', { infer: true }),
+      // port: this.configService.get('database.port', { infer: true }),
+      // username: this.configService.get('database.username', { infer: true }),
+      // password: this.configService.get('database.password', { infer: true }),
+      // database: this.configService.get('database.name', { infer: true }),
       synchronize: this.configService.get('database.synchronize', {
         infer: true,
       }),
